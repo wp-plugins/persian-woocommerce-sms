6ca3089c175e5d5cc453269e0fae259319cc4fca
Persianscript <Persianscript@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-06 06:08:12 +0000

    Added SabzPayamak Webservice
    git-svn-id: https://plugins.svn.wordpress.org/persian-woocommerce-sms/tags/1.4@852424 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== Persian Woocommerce SMS ===
+author: Persian Woocommerce
+author URI: http://www.woocommerce.ir/
+Donate link: http://shop.persianscript.ir/products/woocommerce-sms/
+plugin URI: http://www.woocommerce.ir/plugins.html
+Tags: woocommerce, commerce, e-commerce, commerce, shop, virtual shop, sms, sms notifications, iran, persian, farsi,woocommerce persian
+Requires at least: 3.5
+Tested up to: 3.8.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+SMS notifications From iranian SMS Web services To Woocommerce Customers
+
+== Description ==
+**Persian Woocommerce SMS** نام یک افزونه کاربردی برای سیستم فروشگاه ساز رایگان ووکامرس می باشد که شما را قادر می سازد تا براحتی اقدام به اطلاع رسانی از طریق پیامک به کاربرانتان کنید.
+
+* مدیر فروشگاه در هنگام دریافت سفارش جدید یک پیامک دریافت می کند و همچنین مشتریان در هنگام ارسال سفارش ، تغییر وضعیت سفارش و یا تکمیل وضعیت سفارش پیامک دریافت می کنند. قابل ذکر است این افزونه با آخرین نسخه وردپرس (۳٫۸) و ووکامرس فارسی (۲٫۰٫۲۰) سازگاری کامل دارد.
+
+= Web Services =
+* Web Services Support:
+ * Panizsms.com
+ * Hi-SMS.ir
+ * Farapayamak.ir
+ * PersiaPanel.ir
+ * MTBSMS.ir
+ * SabaPayamak.info
+ * ParandSMS.com
+ * SabzPayamak.ir
+* Send SMS to admin when New Order 
+* Send SMS to customer when New Order
+* Send SMS to customer When Change order status to processing 
+* Send SMS to customer When Change order status to completed 
+* Send SMS to customer When Admin Add a note in order
+* Show credits of SMS Panel Account in Dashboard
+* support from tags in SMS texts: %id%, %order_key%, %billing_first_name%, %billing_last_name%, %billing_company%, %billing_address_1%, %billing_address_2%, %billing_city%, %billing_postcode%, %billing_country%, %billing_state%, %billing_email%, %billing_phone%, %shipping_first_name%, %shipping_last_name%, %shipping_company%, %shipping_address_1%, %shipping_address_2%, %shipping_city%, %shipping_postcode%, %shipping_country%, %shipping_state%, %shipping_method%, %shipping_method_title%, %payment_method%, %payment_method_title%, %order_subtotal%, %order_discount%, %cart_discount%, %order_tax%, %order_shipping%, %order_shipping_tax%, %order_total%, %status%, %shop_name%, %note%.
+
+= Support =
+*  [Persian Support in woocommerce.ir](http://woocommerce.ir/)
+
+== Installation ==
+1. Upload `persian-woocommerce-sms` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Complete Persian Woocommerce SMS Settings
+
+== Frequently asked questions ==
+
+= Where can I find more information and documentation about the plug-in? =
+
+You can read complete documentations on the [woocommerce.ir](http://www.woocommerce.ir)
+= چرا پیامک ها ارسال نمی شوند؟ =
+شما می بایست در تنظیمات ووکامرس ، حتما کشور ایران را به عنوان کشور اصلی انتخاب کنید.
+
+== Screenshots ==
+1. dashboard widget
+2. dashboard widget 2
+3. settings page
+
+== Changelog ==
+= 1.3 =
+* Added: New Web services (5 new sms panel...)
+
+== Upgrade Notice ==
+= 1.3 =
+* Added: New Web services (5 new sms panel...)
+
+==Traducciones ==
+You can read complete documentations on the [woocommerce.ir](http://www.woocommerce.ir)
---
+++ woocommerce-ir.php
@@ -0,0 +1,146 @@
+<?php global $woo_sms; ?>
+
+<div class="wrap woocommerce">
+  <h2>
+   افزونه فارسی پیامک ووکامرس
+  </h2>
+  <?php 
+		settings_errors(); 
+		$tab = 1;
+		$configuracion = get_option('persianscript_sms_woocommerce');
+  ?>
+  <h3><a href="<?php echo $woo_sms['plugin_url']; ?>" title="Woocommerce SMS Plugin"><?php echo $woo_sms['plugin']; ?></a></h3>
+  <p>
+	این افزونه به صورت رایگان عرضه شده است. در صورتی که قصد حمایت از ما را دارید می توانید <a href="http://shop.persianscript.ir/products/woocommerce-sms/" target="_blank"> اینجا کلیک کنید</a>.
+  </p>
+  
+  <form method="post" action="options.php">
+    <?php settings_fields('persianscript_sms_woocommerce_group'); ?>
+    <div class="cabecera"> <a href="http://woocommerce.ir/plugins.html" title="افزونه ارسال پیامک ووکامرس" target="_blank">
+	<span class="imagen"></span></a> </div>
+    <table class="form-table">
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[smswebservice]">
+            سرویس دهنده پیامک:
+          </label>
+          </th>
+        <td class="forminp forminp-number"><select id="persianscript_sms_woocommerce[smswebservice]" name="persianscript_sms_woocommerce[smswebservice]" tabindex="<?php echo $tab++; ?>">
+            <?php
+            $proveedores = array("panizsms" => "PANIZSMS.COM","hi-sms" => "Hi-SMS.ir","mtbsms" => "MTBSMS.ir","parandsms" => "ParandSMS.com","persiapanel" => "PersiaPanel.ir","farapayamak" => "FaraPayamak.ir","sabapayamak" => "SabaPayamak.info","sabzpayamak" => "SabzPayamak.ir");
+            foreach ($proveedores as $valor => $proveedor) 
+            {
+				$chequea = '';
+				if (isset($configuracion['smswebservice']) && $configuracion['smswebservice'] == $valor) $chequea = ' selected="selected"';
+				echo '<option value="' . $valor . '"' . $chequea . '>' . $proveedor . '</option>' . PHP_EOL;
+            }
+            ?>
+          </select></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[smspanelsender]">
+            شماره ارسال کننده:
+          </label>
+          </th>
+        <td class="forminp forminp-number"><input type="text" id="persianscript_sms_woocommerce[smspanelsender]" name="persianscript_sms_woocommerce[smspanelsender]" size="50" value="<?php echo (isset($configuracion['smspanelsender']) ? $configuracion['smspanelsender'] : ''); ?>" tabindex="<?php echo $tab++; ?>" /></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[smspanelusername]">
+            نام کاربری:
+          </label>
+          </th>
+        <td class="forminp forminp-number"><input type="text" id="persianscript_sms_woocommerce[smspanelusername]" name="persianscript_sms_woocommerce[smspanelusername]" size="50" value="<?php echo (isset($configuracion['smspanelusername']) ? $configuracion['smspanelusername'] : ''); ?>" tabindex="<?php echo $tab++; ?>" /></td>
+      </tr>
+	  
+	  <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[smspanelpassword]">
+            رمز عبور:
+          </label>
+          </th>
+        <td class="forminp forminp-number"><input type="password" id="persianscript_sms_woocommerce[smspanelpassword]" name="persianscript_sms_woocommerce[smspanelpassword]" size="50" value="<?php echo (isset($configuracion['smspanelpassword']) ? $configuracion['smspanelpassword'] : ''); ?>" tabindex="<?php echo $tab++; ?>" /></td>
+      </tr>
+	  
+	  
+      
+      
+      
+     
+      
+     
+      
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[userphone]">
+            شماره موبایل مدیر:
+          </label>
+</th>
+        <td class="forminp forminp-number"><input type="number" id="persianscript_sms_woocommerce[userphone]" name="persianscript_sms_woocommerce[userphone]" size="50" value="<?php echo (isset($configuracion['userphone']) ? $configuracion['userphone'] : ''); ?>" tabindex="<?php echo $tab++; ?>" /></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[notificacion]">
+            اطلاع رسانی سفارشات جدید:
+          </label>
+</th>
+        <td class="forminp forminp-number"><input id="persianscript_sms_woocommerce[notificacion]" name="persianscript_sms_woocommerce[notificacion]" type="checkbox" value="1" <?php echo (isset($configuracion['notificacion']) && $configuracion['notificacion'] == "1" ? 'checked="checked"' : ''); ?> tabindex="<?php echo $tab++; ?>" /></td>
+      </tr>
+      
+     
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[mensaje_pedido]">
+           متن پیامک به مسئول سایت:
+          </label>
+          </th>
+        <td class="forminp forminp-number"><textarea id="persianscript_sms_woocommerce[mensaje_pedido]" name="persianscript_sms_woocommerce[mensaje_pedido]" cols="50" rows="5" tabindex="<?php echo $tab++; ?>"><?php echo stripcslashes(isset($configuracion['mensaje_pedido']) ? $configuracion['mensaje_pedido'] : sprintf(__("یک سفارش جدید به شماره %s در ", 'woo_sms'), "%id%") . "%shop_name%" . " افزوده شد."); ?></textarea></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[mensaje_recibido]">
+            متن پیامک به کاربر در هنگام ارسال سفارش جدید:
+          </label>
+		</th>
+        <td class="forminp forminp-number"><textarea id="persianscript_sms_woocommerce[mensaje_recibido]" name="persianscript_sms_woocommerce[mensaje_recibido]" cols="50" rows="5" tabindex="<?php echo $tab++; ?>"><?php echo stripcslashes(isset($configuracion['mensaje_recibido']) ? $configuracion['mensaje_recibido'] : sprintf(__('سفارش شما با شناسه %s با موفقیت در %s ثبت شد. از خرید شما سپاسگذاریم', 'woo_sms'), "%id%", "%shop_name%")); ?></textarea></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[mensaje_procesando]">
+           متن پیامک در هنگام تغییر وضعیت سفارش:
+          </label>
+				</th>
+				<td class="forminp forminp-number"><textarea id="persianscript_sms_woocommerce[mensaje_procesando]" name="persianscript_sms_woocommerce[mensaje_procesando]" cols="50" rows="5" tabindex="<?php echo $tab++; ?>"><?php echo stripcslashes(isset($configuracion['mensaje_procesando']) ? $configuracion['mensaje_procesando'] : sprintf(__('از سفارش شما سپاسگذاریم!. سفارش شما به شماره %s هم اکنون در وضعیت پردازش قرار دارد ', 'woo_sms'), "%id%")); ?></textarea></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[mensaje_completado]">
+            متن پیامک در هنگام تکمیل سفارش:
+          </label>
+		</th>      
+		<td class="forminp forminp-number"><textarea id="persianscript_sms_woocommerce[mensaje_completado]" name="persianscript_sms_woocommerce[mensaje_completado]" cols="50" rows="5" tabindex="<?php echo $tab++; ?>"><?php echo stripcslashes(isset($configuracion['mensaje_completado']) ? $configuracion['mensaje_completado'] : sprintf(__('از سفارش شما سپاسگذریم. سفارش شما به شماره %s هم اکنون در وضعیت تکمیل شده قرار دارد ', 'woo_sms'), "%id%")); ?></textarea></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row" class="titledesc"> <label for="persianscript_sms_woocommerce[mensaje_nota]">
+            متن پیامک در هنگام نوشتن پیغام دلخواه در سفارش:
+          </label>
+</th>
+        <td class="forminp forminp-number"><textarea id="persianscript_sms_woocommerce[mensaje_nota]" name="persianscript_sms_woocommerce[mensaje_nota]" cols="50" rows="5" tabindex="<?php echo $tab++; ?>"><?php echo stripcslashes(isset($configuracion['mensaje_nota']) ? $configuracion['mensaje_nota'] : sprintf(__('یک پیغام جدید در سفارش شماره %s ارسال شده است. متن پیغام: ', 'woo_sms'), "%id%") . "%note%"); ?></textarea></td>
+      </tr>
+    </table>
+    <p class="submit">
+      <input class="button-primary" type="submit" value="ذخیره تغییرات"  name="submit" id="submit" tabindex="<?php echo $tab++; ?>" />
+    </p>
+  </form>
+</div>
+<script type="text/javascript">
+jQuery(document).ready(function($) {	
+	$('select').on('change', function () {
+		control($(this).val());
+	});
+
+	var control = function(capa) {
+		var proveedores= new Array();
+		<?php foreach($proveedores as $indice => $valor) echo "proveedores['$indice'] = '$valor';" . PHP_EOL; ?>
+		
+		for (var valor in proveedores) {
+    		if (valor == capa) $('.' + capa).show();
+			else $('.' + valor).hide();
+		}
+	};
+	control($('select').val());
+});
+</script> 
+
+
